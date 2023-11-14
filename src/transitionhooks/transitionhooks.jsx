d3b1c659cabff74ec4c30c52623d690b3ecf4b2e
pritamcn <you@example.com>
2023-11-14 18:55:03 +0530

    updated

--- src/transitionhooks/transitionhooks.jsx
+++ src/transitionhooks/transitionhooks.jsx
@@ -1,9 +1,36 @@
+import { useDeferredValue, useState, useTransition, useEffect } from "react"
 
+const bigArray = [...Array(20000).keys()]
 
 const Transitionhooks = () => {
-  return (
-    <div>transitionhooks</div>
-  )
-}
+    const [inputValue, setInputValue] = useState('')
+    const [list, setList] = useState(bigArray)
+    const [isPending, startTransition] = useTransition()
+    const deferredInput = useDeferredValue(inputValue)
 
+    const handleInput = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    useEffect(() => {
+        startTransition(() => {
+            const filtered = bigArray.filter(item => item.toString().includes(deferredInput))
+            setList(filtered)
+        })
+    }, [deferredInput])
+
+
+    const content = <section style={isPending ? { opacity: 0.4 } : null}>
+        <p>Searching for: {deferredInput || "All"}</p>
+        {isPending ? <p>Loading...</p> : null}
+        <ul>{list.map(item => <li key={item}>{item}</li>)}</ul>
+    </section>
+
+    return (
+        <div className="App">
+            <input type="text" value={inputValue} onChange={handleInput} />
+            {content}
+        </div>
+    );
+}
 export default Transitionhooks
